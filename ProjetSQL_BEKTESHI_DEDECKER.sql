b391706397e5b775d43abea2d8688516cbbf071e
alban <alban.bekteshi@student.vinci.be>
2020-10-30 15:12:14 +0100

    creat

--- ProjetSQL_BEKTESHI_DEDECKER.sql
+++ ProjetSQL_BEKTESHI_DEDECKER.sql
@@ -1,2 +1,33 @@
-DROP SCHEMA IF EXISTS preprojet CASCADE;
-CREATE SCHEMA preprojet;
+DROP SCHEMA IF EXISTS projet CASCADE;
+CREATE SCHEMA projet;
+
+CREATE TABLE projet.formations (
+	id_formation SERIAL PRIMARY KEY,
+	nom VARCHAR(100) NOT NULL CHECK (nom<>''),
+	ecole VARCHAR(100) NOT NULL CHECK (ecole<>'')
+);
+
+CREATE TABLE projet.blocs (
+	id_bloc SERIAL PRIMARY KEY,
+	code_bloc CHARACTER(4)
+		CHECK(code_bloc SIMILAR TO '[0-9]BIN'),
+	id_formation INTEGER REFERENCES projet.formations (id_formation) NOT NULL
+);
+
+CREATE TABLE projet.utilisateurs(
+	id_utilisateur SERIAL PRIMARY KEY,
+	nom_utilisateur VARCHAR(100) NOT NULL UNIQUE CHECK(nom_utilisateur<>''),
+	email VARCHAR(100) NOT NULL UNIQUE CHECK(email<>''),
+	mot_de_passe VARCHAR(100) NOT NULL (mot_de_passe<>''),
+	id_bloc INTEGER REFERENCES projet.blocs (id_bloc) NOT NULL
+);
+
+CREATE TABLE projet.examens(
+	code_examen VARCHAR(6) PRIMARY KEY UNIQUE
+		CHECK(code_examen SIMILAR TO 'IPL[0-9][0-9][0-9]'),
+	nom VARCHAR(100) NOT NULL CHECK(nom<>''),
+	id_bloc INTEGER REFERENCES projet.blocs (id_bloc) NOT NULL,
+	duree INT(3) NOT NULL, 
+	date timestamp without time zone NOT NULL,
+	support CHAR(1) NOT NULL CHECK (support='m' || support='e' )
+);
