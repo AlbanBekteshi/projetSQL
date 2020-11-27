549617f6f5b1cc5e1322d22f23d4b1416f65a86d
AdriendeDecker <adriendedecker@yahoo.fr>
2020-11-27 09:55:31 +0100

    Refactor - externalisation des commandes du scénario

--- ProjetSQL_BEKTESHI_DEDECKER.sql
+++ ProjetSQL_BEKTESHI_DEDECKER.sql
@@ -58,29 +58,6 @@ CREATE TABLE projet.locaux_examens (
 INSERTS
 */
 
-INSERT INTO projet.formations (nom, ecole) 
-	VALUES ('Bachelier en Informatique de Gestion','IPL');
-INSERT INTO projet.blocs(id_bloc,code_bloc,id_formation)
-	VALUES (DEFAULT,'Bloc 1',1);
-INSERT INTO projet.blocs(id_bloc,code_bloc,id_formation)
-	VALUES (DEFAULT,'Bloc 2',1);
-INSERT INTO projet.examens (code_examen,nom,id_bloc,duree,support)
-	VALUES ('IPL100','APOO',1,120,'e');
-INSERT INTO projet.examens (code_examen,nom,id_bloc,duree,support)
-	VALUES ('IPL150','ALGO',1,60,'m');
-INSERT INTO projet.examens (code_examen,nom,id_bloc,duree,support)
-	VALUES ('IPL200','JAVASCRIPT',2,120,'m');
-INSERT INTO projet.locaux (id_local,capacite,machine)
-	VALUES ('A017',2,'o');
-INSERT INTO projet.locaux (id_local,capacite,machine)
-	VALUES ('A019',1,'o');
-INSERT INTO projet.utilisateurs(id_utilisateur,nom_utilisateur,email,mot_de_passe,id_bloc)
-	VALUES (DEFAULT,'Damas','Damas@email.be','DamasCode',1);
-INSERT INTO projet.utilisateurs(id_utilisateur,nom_utilisateur,email,mot_de_passe,id_bloc)
-	VALUES (DEFAULT,'Ferneeuw','Ferneeuw@email.be','FerneeuxCode',2);
-INSERT INTO projet.utilisateurs(id_utilisateur,nom_utilisateur,email,mot_de_passe,id_bloc)
-	VALUES (DEFAULT,'Cambron','Cambron@email.be','CambronCode',2);
-
 --SELECT projet.ajouterLocal('2b1',5,'o');
 --SELECT projet.inscriptionUtilisateur('admin','admin@vinci.be','123',1);
 --SELECT projet.ajoutExamen('IPL123','SQL Exam',1,150,'e');
