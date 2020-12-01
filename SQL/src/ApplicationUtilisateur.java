6abf7c0facf9a9e4be6730bd1dc9d18078d0e9c0
AdriendeDecker <adriendedecker@yahoo.fr>
2020-12-01 23:44:16 +0100

    java inscirptionbloc

--- SQL/src/ApplicationUtilisateur.java
+++ SQL/src/ApplicationUtilisateur.java
@@ -121,9 +121,9 @@ public class ApplicationUtilisateur {
 		// TODO
 		try {
 			ajouterInscriptionExamenBloc.setInt(1, this.idUtilisateur);
-			try(ResultSet rs = ajouterInscriptionExamen.executeQuery()){
+			try(ResultSet rs = ajouterInscriptionExamenBloc.executeQuery()){
 				while(rs.next()) {
-					if(rs.getBoolean("boolean")) {
+					if(rs.getBoolean(1)) {
 						System.out.println("Inscription reussie !");
 					}else {
 						System.out.println("Il y a eu un problème lors de l'inscription à tous les examens du bloc");
@@ -280,7 +280,7 @@ public class ApplicationUtilisateur {
 			obtenirUtilisateurDepuisIdUtilisateur = conn.prepareStatement("SELECT * FROM projet.utilisateurs WHERE id_utilisateur = ?;");
 			visualiserExamenBloc = conn.prepareStatement("SELECT * FROM projet.examens WHERE id_bloc = ?");
 			ajouterInscriptionExamen = conn.prepareStatement("SELECT * FROM projet.ajouterInscriptionExamen(?,?);");
-			ajouterInscriptionExamenBloc = conn.prepareStatement("SELECT projet.ajouterInscriptionExamenBloc(?);");
+			ajouterInscriptionExamenBloc = conn.prepareStatement("SELECT * FROM projet.ajouterInscriptionExamenBloc(?);");
 			afficherHoraireUtilisateur = conn.prepareStatement("SELECT * FROM projet.obtenirHoraireExamen(?) t(code_examen VARCHAR,nom VARCHAR, dateDebut TIMESTAMP, duree INTEGER, locaux VARCHAR);");
 		} catch(SQLException e) {
 			System.out.println("Erreur lors de la preparation des statement");
