688fb4740a79cbceb6f10f901b7d6578d81db4a1
AdriendeDecker <adriendedecker@yahoo.fr>
2020-12-04 10:32:29 +0100

    Init debut demo

--- ProjetSQL_BEKTESHI_DEDECKER.sql
+++ ProjetSQL_BEKTESHI_DEDECKER.sql
@@ -360,16 +360,6 @@ INSERT INTO projet.blocs(id_bloc,code_bloc,id_formation)
 	VALUES (DEFAULT,'Bloc 1',1);
 INSERT INTO projet.blocs(id_bloc,code_bloc,id_formation)
 	VALUES (DEFAULT,'Bloc 2',1);
-INSERT INTO projet.examens (code_examen,nom,id_bloc,duree,support)
-	VALUES ('IPL100','APOO',1,120,'e');
-INSERT INTO projet.examens (code_examen,nom,id_bloc,duree,support)
-	VALUES ('IPL150','ALGO',1,60,'m');
-INSERT INTO projet.examens (code_examen,nom,id_bloc,duree,support)
-	VALUES ('IPL200','JAVASCRIPT',2,120,'m');
-INSERT INTO projet.locaux (id_local,capacite,machine)
-	VALUES ('A017',2,'o');
-INSERT INTO projet.locaux (id_local,capacite,machine)
-	VALUES ('A019',1,'o');
 
 --Tous les mdp sont 123
 INSERT INTO projet.utilisateurs(id_utilisateur,nom_utilisateur,email,mot_de_passe,id_bloc)
